e77c36f67a19d89cc55bb5ef495e920f5ae9f2f8
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-02-07 18:56:44 +0100

    debug config

--- source/hic_hal/nordic/nrf5340/sdk.c
+++ source/hic_hal/nordic/nrf5340/sdk.c
@@ -11,7 +11,6 @@ void sdk_init()
 {
 
 	/* Enable instruction trace */
-	/*
 	NRF_TAD_S->ENABLE = TAD_ENABLE_ENABLE_Msk;
 	NRF_TAD_S->CLOCKSTART = TAD_CLOCKSTART_START_Msk;
 	NRF_TAD_S->PSEL.TRACECLK   = TAD_PSEL_TRACECLK_PIN_Traceclk;
@@ -25,7 +24,7 @@ void sdk_init()
 			|= (GPIO_PIN_CNF_MCUSEL_TND << GPIO_PIN_CNF_MCUSEL_Pos)
 			| (GPIO_PIN_CNF_DRIVE_E0E1 << GPIO_PIN_CNF_DRIVE_Pos);
 	NRF_TAD_S->TRACEPORTSPEED = TAD_TRACEPORTSPEED_TRACEPORTSPEED_64MHz;
-	*/
+
 	if (!nrfx_clock_hfclk_is_running()) {
 		nrfx_clock_hfclk_start();
 	}
