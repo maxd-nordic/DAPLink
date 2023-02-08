f87bccd0cf016453dfda2ee3d4f2c2b75a5f48a6
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-02-08 12:07:51 +0100

    non-working config for hic-nrf52840

--- source/hic_hal/device.h
+++ source/hic_hal/device.h
@@ -44,6 +44,8 @@
 #include "M480.h"
 #elif defined (INTERFACE_NRF52820)
 #include "nrf52820.h"
+#elif defined (INTERFACE_NRF52840)
+#include "nrf52840.h"
 #elif defined (INTERFACE_NRF5340)
 #include "nrf5340_application.h"
 #else
